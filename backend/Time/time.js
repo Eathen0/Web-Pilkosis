b3abcb91c2c6bb24b4f8486869c3ebcfce2087b7
AlbabRiziq <albabriziq@gmail.com>
2023-04-06 13:03:59 +0700

    update sesuatu

--- backend/Time/time.js
+++ backend/Time/time.js
@@ -1,5 +1,28 @@
-let waktu = new Date();
+const Time = () => {
+  let waktu = new Date();
 
-const tahun = waktu.getFullYear();
-const bulan = waktu.getMonth();
-const hari = waktu.getDay();
+  const nama_hari = [
+    "Minggu",
+    "Senin",
+    "Selasa",
+    "Rabu",
+    "Kamis",
+    "Jumat",
+    "Sabtu",
+  ];
+
+  const tahun = waktu.getFullYear();
+  const bulan = waktu.getMonth();
+  const hari = nama_hari[waktu.getDay()];
+  const tanggal = waktu.getDate();
+  const jam = waktu.getHours();
+  const menit = waktu.getMinutes();
+  const detik = waktu.getSeconds();
+
+  const waktunya = `${hari} ${tanggal}-${bulan}-${tahun} ${jam}:${menit}:${detik}`;
+
+  return waktunya;
+};
+
+// console.log(Time());
+export default Time;
