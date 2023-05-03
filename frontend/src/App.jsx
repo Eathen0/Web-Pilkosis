21a4c532157d22e7a28c08f14b93bcc821eace0c
AlbabRiziq <albabriziq123@gmail.com>
2023-05-03 16:02:38 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import LoginPage from './login-page/login'
-import UserPage from './user-page/user'
+import LoginPage from "./login-page/login";
+import UserPage from "./user-page/user";
 
 function App() {
-  const [swich, setSwich] = useState(false)
-  const [pageName, setPageName] = useState("login")
-  
-  
+  const [swich, setSwich] = useState(false);
+  const [pageName, setPageName] = useState("login");
+
   function handle() {
-    swich ? setPageName("login") : setPageName("pilkosis")
-    setSwich(!swich)
+    swich ? setPageName("login") : setPageName("pilkosis");
+    setSwich(!swich);
   }
 
   useEffect(() => {
-    document.title = pageName
-  }, [pageName])
+    document.title = pageName;
+  }, [pageName]);
 
   return (
     <div className="App">
-      {swich ? <UserPage onClick={handle}/> : <LoginPage click={handle}/>}
+      {swich ? <UserPage onClick={handle} /> : <LoginPage click={handle} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/login-page/login.jsx
+++ frontend/src/login-page/login.jsx
@@ -1,43 +1,75 @@
-import logoSmk from './login-aset/logo-smk.png'
-import logoOsis from './login-aset/logo-osis.png'
+import axios from "axios";
+import { useState } from "react";
+import logoSmk from "./login-aset/logo-smk.png";
+import logoOsis from "./login-aset/logo-osis.png";
 
 export default function LoginPage({ click }) {
-    return (
-        <div className={`flex items-center w-full h-screen bg-login bg-cover flex-col justify-start gap-0 sm:gap-4 sm:justify-center`}>
-            <div className="flex items-center m-4 gap-52 sm:m-0 sm:gap-10">
-                <img src={logoSmk} alt="logo smkn 1 kebumen" className="bg-white rounded-full w-16 h-16" />
-                <img src={logoOsis} alt="logo osis" className="w-16 h-[4.5rem]" />
-            </div>
-            <div className="flex items-center shadow-2xl shadow-black justify-center flex-col backdrop-blur-sm bg-slate-950/60 p-5 rounded-xl gap-5 w-[25rem] h-[35rem] sm:gap-10 sm:w-[40rem] sm:h-[30rem]">
-                <h2 className="text-xl text-center font-bold mb-2 text-white w-72 sm:w-[30rem] h-10 border-b-[1px] border-white">Login</h2>
-                <form className='w-60 sm:w-96'>
-                    <div className="mb-4">
-                        <label className="block text-white font-bold mb-2">Username</label>
-                        <input
-                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
-                        id="username"
-                        type="text"
-                        placeholder="Username"/>
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-white font-bold mb-2">Password</label>
-                        <input
-                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
-                        id="password"
-                        type="password"
-                        placeholder="password"/>
-                    </div>
-                    <div className="flex items-center justify-between">
-                        <button
-                        className="bg-blue-500 hover:bg-cyan-500 transition text-white font-bold py-2 px-4 rounded-lg"
-                        type="button"
-                        onClick={click}
-                        >
-                        Login
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+
+  const login = () => {
+    axios({
+      method: "GET",
+      url: "http://localhost:8080/login",
+      params: {
+        username,
+        password,
+      },
+    }).then((res) => console.log(res));
+  };
+
+  return (
+    <div
+      className={`flex items-center w-full h-screen bg-login bg-cover flex-col justify-start gap-0 sm:gap-4 sm:justify-center`}
+    >
+      <div className="flex items-center m-4 gap-10 justify-center">
+        <img
+          src={logoSmk}
+          alt="logo smkn 1 kebumen"
+          className="bg-white rounded-full w-16 h-16"
+        />
+        <img src={logoOsis} alt="logo osis" className="w-16 h-[4.5rem]" />
+      </div>
+      <div className="flex items-center shadow-2xl shadow-black justify-center flex-col backdrop-blur-sm bg-slate-950/60 p-5 rounded-xl gap-5 w-[25rem] h-[35rem] sm:gap-10 sm:w-[40rem] sm:h-[30rem]">
+        <h2 className="text-xl text-center font-bold mb-2 text-white w-72 sm:w-[30rem] h-10 border-b-[1px] border-white">
+          Login
+        </h2>
+        <form className="w-60 sm:w-96">
+          <div className="mb-4">
+            <label className="block text-white font-bold mb-2">Username</label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight"
+              id="username"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-6">
+            <label className="block text-white font-bold mb-2">Password</label>
+            <input
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight"
+              id="password"
+              type="password"
+              placeholder="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-cyan-500 transition text-white font-bold py-2 px-4 rounded-lg"
+              type="button"
+              onClick={login}
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
