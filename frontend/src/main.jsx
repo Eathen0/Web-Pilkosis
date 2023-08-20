c4ef94b33363de10e44bd60560006c1bc24e85cc
Gamer_093 <wandgenius6@gmail.com>
2023-08-20 16:59:21 +0700

    p

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-        
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  redirect,
+  useNavigate,
+} from "react-router-dom";
+import "./index.css";
+import User from "./Pages/User/userComponent/user";
+import Admin from "./Pages/Admin/adminComponent/admin";
+import LoginPage from "./Components/login-page/login";
+
+const data = { tes: "jfd" };
+
+export const ContextData = createContext();
+export const ContextLogin = createContext();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   < >
