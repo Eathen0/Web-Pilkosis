388bba961e8736b48202f51599ed92ba37cdb66a
Gamer_093 <wandgenius6@gmail.com>
2023-09-10 12:01:36 +0700

    router update

--- frontend/src/Pages/User/About/about.jsx
+++ frontend/src/Pages/User/About/about.jsx
@@ -1,7 +1,28 @@
-import img from "../../../assets/img.png";
+import c1 from './about-assets/c1.jpeg'
+import c2 from './about-assets/c2.jpeg'
+import c3 from './about-assets/c3.jpeg'
 
 export default function About() {
-  const cartsMany = [...Array(6)];
+  const cartsMany = [
+    {
+      foto: c1,
+      nama: 'Riziq Lili Ulil Albab',
+      kelas: 'X',
+      jurusan: 'PPLG'
+    },
+    {
+      foto: c2,
+      nama: 'Putri Nurinita Sari',
+      kelas: 'X',
+      jurusan: 'PPLG'
+    },
+    {
+      foto: c3,
+      nama: 'Muhammad Adam Maulana Nuradyan',
+      kelas: 'X',
+      jurusan: 'PPLG'
+    },
+  ];
 
   return (
     <div className="pt-20">
@@ -14,15 +35,20 @@ export default function About() {
               className="p-2 hover:shadow-[3px_3px_10px_black] text-white transition-all duration-500 w-64 h-64 bg-slate-400 rounded-xl overflow-hidden flex flex-col"
             >
               <img
-                className="hover:opacity-0 transition-all duration-500 absolute shadow-[1px_0_5px_black] w-[15rem] h-[15rem]"
-                src={img}
+                className="hover:opacity-0 object-cover transition-all duration-500 absolute shadow-[1px_0_5px_black] w-[15rem] h-[15rem]"
+                src={el.foto}
               />
               <div className="flex flex-col w-full h-full justify-center">
-                <h1 className="font-bold text-lg mt-3 ml-3">Fulan</h1>
-                <div className="m-2 p-2 grid grid-cols-3 shadow-lg items-center w-full">
-                  <p>kelas</p> : <p>mnmnm</p>
-                  <p>jurusan</p> : <p>mnmnm</p>
-                  <p>Quote</p> : <p>mnmnm</p>
+                <h1 className="font-bold text-lg mt-3 ml-3">{el.nama}</h1>
+                <div className="m-2 p-2 flex gap-2 shadow-lg items-center w-full">
+                  <ul>
+                    <li>kelas</li>
+                    <li>jurusan</li>
+                  </ul>
+                  <ul>
+                    <li> : {el.kelas}</li>
+                    <li> : {el.jurusan}</li>
+                  </ul>
                 </div>
               </div>
             </div>
